b819d3ae5647cf3ae4a4f9cfed0e5c3967fd9d6e
Tran Dieu <tracpham2210@gmail.com>
2025-10-24 21:17:14 +0700

    update

--- my-app/src/types/api.js
+++ my-app/src/types/api.js
@@ -1,4 +1,3 @@
 export const BASE_API = "http://localhost:3002";
 export const ADMIN_API = "http://localhost:3002/admin";
-export const CLIENT_API = "http://localhost:3002/client";
 export const AUTH_API = "http://localhost:3002/auth";
